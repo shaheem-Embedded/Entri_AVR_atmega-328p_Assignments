09de39e7b692e6d6453f469c78d568f79184cc4d
Shaheem Ahamed <shaheem.ahamed@gmail.com>
2026-08-03 23:34:38 +0400

    Added DC motor Speed control using PWM

---
+++ Assignment-2/9.Motor_speed_control_PWM/Motor_speed_control_PWM/Motor_speed_control_PWM/library.c
@@ -0,0 +1,16 @@
+/*
+ * Motor_speed_control_PWM.c
+ *
+ * Created: 03-Aug-26 11:32:40 PM
+ * Author : SHAHEEM
+ */
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+    return 0;
+}
+
---
+++ Assignment-2/9.Motor_speed_control_PWM/Motor_speed_control_PWM/Motor_speed_control_PWM/main.c
@@ -0,0 +1,18 @@
+/*
+ * Motor_speed_control_PWM.c
+ *
+ * Created: 03-Aug-26 11:32:38 PM
+ * Author : SHAHEEM
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1)
+    {
+    }
+}
+
